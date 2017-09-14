2d3fe810df71a74cb194ff1966b833be0c5a3431
Shannon Nichols <shannonsn19@gmail.com>
2017-09-14 17:39:10 +0200

    created routes and filtered through the database for specific information

--- index.js
+++ index.js
@@ -1,9 +1,91 @@
-var express = require('express')
+const express = require('express')
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 app = express();
+
+const models = require('./models/shoesSchemaModel');
+
+app.use(express.static('public'))
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+//gets all the data from the database
+app.get('/api/shoes', function(req, res) {
+
+    models.find({}, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result)
+        }
+    })
+
+})
+
+//get the the shoe brand from the database
+app.get('/api/shoes/brand/:brandname', function(req, res) {
+
+    models.find({
+        brand: req.params.brandname
+    }, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else if (result) {
+            res.json(result)
+        } else {
+          res.send('not found')
+        }
+    })
+
+})
+
+//get all the shoes sizes from the database
+app.get('/api/shoes/size/:size', function(req, res){
+
+  models.find({
+    size: req.params.size
+  }, function(err, result){
+    if (err) {
+      console.log(err);
+    } else if (result) {
+      res.json(result)
+    } else {
+      res.send('not found')
+    }
+  })
+})
+
+//get all the shoes size and brand from the database
+app.get('/api/shoes/brand/:brandname/size/:size', function(req,res){
+  models.findOne({
+    brand: req.params.brandname,
+    size: req.params.size
+  }, function(err ,result){
+    if (err){
+      console.log(err);
+    }else if (result) {
+      res.json(result);
+    } else {
+      res.send('not found')
+    }
+  })
+})
+
 
 
 var port = process.env.PORT || 7070;
 
-app.listen(port, function(){
-  console.log("we are good to go at" , port);
+app.listen(port, function() {
+    console.log("we are good to go at", port);
 })
---
+++ models/shoesSchemaModel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/shoesCatalogue";
+
+mongoose.connect(mongoURL, {
+    useMongoClient: true
+}, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("This is where the magic happens");
+    }
+});
+
+var shoesCatalogueSchema = new  mongoose.Schema({
+    color : String,
+    brand : String,
+    price : Number,
+    size : Number,
+    in_stock : Number
+
+});
+
+const shoes = mongoose.model('shoes' , shoesCatalogueSchema);
+
+
+module.exports = shoes;
